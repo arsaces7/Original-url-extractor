859fb09f18d5a89e2315678a58b4f45287520b88
arsaces7 <55933676+arsaces7@users.noreply.github.com>
2024-05-25 03:51:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
+// sw.js
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
         '/',
-        'index.html',
-        'manifest.json',
-        'icon.png'
+        '/index.html',
+        '/manifest.json',
+        '/icon.png'
       ]);
     })
   );
